d89447c1bf0d6b3953bade7155ab19ca7200e60e
RealZST <53246001+RealZST@users.noreply.github.com>
2024-01-08 14:37:52 +0800

    Update train.py

--- TD3/train.py
+++ TD3/train.py
@@ -16,7 +16,7 @@ from gazebo_env import envmodel
 
 '''''''''''setting before running'''''''''''
 ON_TRAIN = 1  # train or not. if train, world in sitl.launch is 'train_env'
-aDRL_TEST = 1  # test DGlobal or not
+aDRL_TEST = 0  # test DGlobal or not
 oDRL_TEST = 0  # test TD3 or not
 test_env = 'Env2'  # consistent with the world setting in sitl.launch(test_env1,test_env2...)
 sim_speed = 10  # simulation acceleration factor, corresponding to PX4_SIM_SPEED_FACTOR in sitl.launch
